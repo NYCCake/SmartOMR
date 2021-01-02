60dd59e681cdb1b9ada5009612b43e8350339193
estesca11 <cjo7897@gmail.com>
2021-01-02 21:45:34 +0900

    cccccccccccccc

--- a.py
+++ a.py
@@ -1,6 +1,11 @@
-import cv2
-
-img=cv2.imread('C:\sample.png',cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def very_important_function(
+    template: str,
+    *variables,
+    file: os.PathLike,
+    engine: str,
+    header: bool = True,
+    debug: bool = False
+):
+    """Applies `variables` to the `template` and writes to `file`."""
+    with open(file, "w") as f:
+        ...
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import cv2
-print(cv2.__version__)
-print('aaa')
+
+src = cv2.imread("D:\samplesheet.jpg", cv2.IMREAD_COLOR)
+
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+ret, dst = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+cv2.imshow("dst", dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
